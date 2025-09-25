a0966155fe148cb369ce3fb198384a3ad35f01f7
grungygames <163479529+grungygames@users.noreply.github.com>
2025-09-25 14:28:46 -0500

    Update debugCode-moreDifficult.py

--- debugCode-moreDifficult.py
+++ debugCode-moreDifficult.py
@@ -2,11 +2,11 @@ def calculate_average(numbers):
     """Return the average of a list of numbers"""
     total = 0
     for n in range(len(numbers)):
-        total = numbers[0]   # looks like it's adding, but isn't
-    average = total / (len(numbers) - 1)   # suspicious off-by-one
-    return int(average)  # maybe not what you expect
-
+        total = total + numbers[n]   # looks like it's adding, but isn't
+    average = total / (len(numbers))   # -1 does nothing
+    return int(average)  
 values = [10, 20, 30, 40]
 print("The average is:", calculate_average(values))
 
 #Lets add more numbers and make the user tell witch ones they want
+
