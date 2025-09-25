e8f11b8018b1d618a37cfca6aabc031787cb5784
grungygames <163479529+grungygames@users.noreply.github.com>
2025-09-25 14:09:09 -0500

    Update debugCode.py

--- debugCode.py
+++ debugCode.py
@@ -1,14 +1,15 @@
 def find_max(numbers):
     """Return the maximum number in the list"""
-    max_num = -20
+    max_num = -20 #if zero does n0t work for neg numbers in list
     for num in numbers:
         if num >= max_num:
             max_num = num
-    return max_num
+    return max_num #max_Num
 
 
 values = [-3, -7, -1, -12]
 print("The max value is:", find_max(values))
+
 
 
 
