2cd5893663b0367d6e1063eed5b2e722ab24719b
grungygames <163479529+grungygames@users.noreply.github.com>
2025-09-25 14:00:23 -0500

    Update debugCode.py
    -3 is less then 0

--- debugCode.py
+++ debugCode.py
@@ -1,6 +1,6 @@
 def find_max(numbers):
     """Return the maximum number in the list"""
-    max_num = 0
+    max_num = -20
     for num in numbers:
         if num >= max_num:
             max_num = num
@@ -9,4 +9,5 @@ def find_max(numbers):
 
 values = [-3, -7, -1, -12]
 print("The max value is:", find_max(values))
+
 
